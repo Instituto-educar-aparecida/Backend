15daa131106a98e937f1eb4090c7a94873e55d9a
Kelverton <kevinsousa201@gmail.com>
2026-02-12 21:26:36 +0100

    Index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5173;
+
+// Configuração de Integração (Sua área!)
+app.use(cors({
+  origin: 'http://localhost:5174', // Libera o Frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+app.use(express.json());
+
+// Rota de teste para a reunião
+app.get('/', (req, res) => {
+  res.json({ mensagem: "🚀 API do Instituto Educar Online e Integrada!" });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`[BACKEND] Servidor rodando na porta ${PORT}`);
+});
+
