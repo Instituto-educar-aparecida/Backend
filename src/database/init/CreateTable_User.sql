12fc57757aeff1f65bc730a34d6dd46e90c18764
Vinicius Veras <viniciusxveras@gmail.com>
2026-04-24 21:12:27 -0300

    feat: Adição das tabelas ProgressoAtividade de ProgressoAula, além da atualização da tabela User

--- src/database/init/CreateTable_User.sql
+++ src/database/init/CreateTable_User.sql
@@ -1,13 +1,15 @@
-CREATE TABLE IF NOT EXISTS public."user"
-(
+-- DROP TABLE IF EXISTS public."user";
+
+CREATE TABLE IF NOT EXISTS public."user"(
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(45) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    hash TEXT NOT NULL,
-    role VARCHAR(20) NOT NULL
+    role INT NOT NULL,
+    senhahash TEXT NOT NULL,
+    ativa BOOLEAN DEFAULT TRUE
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."user"
-    OWNER to "EducarDev";
+OWNER TO "EducarDev";
